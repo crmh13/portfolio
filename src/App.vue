31f41b9f8cf8dc3b912b6371adaeb771278e725e
crmh <t260robo@gmail.com>
2020-02-15 13:54:37 +0900

    トップページレイアウト作成

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
+    <nav>
+      <ul>
+        <li>ステータス</li>
+        <li>しごと</li>
+        <li>せいさく</li>
+      </ul>
+      <div>
+        <p class="txt-left">TIME</p>
+        <p id="time">00:00</p>
+      </div>
+    </nav>
   </div>
 </template>
 
 <script>
+import "./styles/common.css"
 export default {
   name: 'App'
 }
@@ -17,7 +28,46 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: blue;
+  color: #ffffff;
+  width: 960px;
+  height: 800px;
+  margin: auto;
+  border: 5px solid #fff;
+  border-radius: 5px;
+  box-sizing: border-box;
+  display: flex;
+}
+
+ul {
+  margin: 0;
+  height: 600px;
+  border-top: 5px solid #fff;
+  border-left: 5px solid #fff;
+  border-bottom: 5px solid #fff;
+  border-radius: 5px 0 0 5px;
+  padding-top: 15px;
+}
+ul li {
+  list-style-type: none;
+  text-align: left;
+  font-size: 20px;
+  padding: 5px 0;
+}
+
+nav {
+  width: 180px;
+  top: 0;
+  right: 0;
+}
+
+nav div {
+  border-top: 5px solid #fff;
+  border-left: 5px solid #fff;
+  border-bottom: 5px solid #fff;
+  border-radius: 5px 0 0 5px;
+}
+nav div p {
+  margin: 5px;
 }
 </style>
---
+++ src/components/TopPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+    <h1>Port Folio</h1>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'TopPage'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 0;
+  height: 100%;
+  line-height: 800px;
+  font-size: 65px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
