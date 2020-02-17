b0d73de6c7cbad3e3edee05ada13e6f2b89d38d7
crmh <t260robo@gmail.com>
2020-02-18 00:17:08 +0900

    仕事ページの作成

---
+++ src/components/WorkPage.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <article>
+      <ul>
+        <li v-for="workList in workLists" :key="workList.id" 
+          :class="{'active': workId === workList.id}"
+          @click="select(workList.id)">{{ workList.work }}</li>
+      </ul>
+      <div v-if="workId !== null" class="content">
+        <p>{{ workLists[workId].content }}</p>
+        <p>使用した言語<br>{{ workLists[workId].lang}}</p>
+        <p v-if="workLists[workId].db !== ''">使用したDB<br>{{ workLists[workId].db }}</p>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Work',
+  data: function() {
+    return {
+      workLists: [
+        { 
+          id: 0,
+          work: '官公庁のWebサイト運用・保守',
+          content: `国税庁、内閣府、東京都などのWebページのアクセシビリティ対応、更新等を行う。\n特にアクセシビリティ対応に力を入れ、構文エラーがないように気を付けてコーディングを行った。`,
+          lang: 'HTML、CSS、VBA、JavaScript(多少)',
+          db: ''
+        },
+        { 
+          id: 1,
+          work: '業務系システム開発・保守',
+          content: '帳票出力機能改修、CSVダウンロード機能開発、メールアドレス管理機能開発や、問い合わせシステムの保守としてエラーログの調査等を行う。\n対応案の作成、見積、設計、実装、テストまで一通りの工程を経験。',
+          lang: 'Java、JavaScript、VBA',
+          db: 'Oracle Database'
+        }
+      ],
+      workId: null
+    }
+  },
+  methods: {
+    select: function(id) {
+      this.workId = id;
+    }
+  }
+}
+</script>
+
+<style scoped>
+article {
+  display: flex;
+}
+
+ul {
+  width: 250px;
+  height: 780px;
+  margin: 0;
+  padding-top: 10px;
+  text-align: left;
+  border-right: 5px solid #fff;
+}
+
+ul li {
+  padding: 5px;
+  cursor: pointer;
+}
+
+ul li:hover {
+  background: rgba(255, 255, 255, 0.2);
+}
+
+.active {
+  text-decoration: underline;
+}
+
+.content {
+  width: 475px;
+  text-align: left;
+  padding: 10px;
+}
+
+.content p {
+  white-space: pre-wrap;
+  margin: 10px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import TopPage from '@/components/TopPage'
 import StatusPage from '@/components/StatusPage'
+import WorkPage from '@/components/WorkPage'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/status',
       name: 'StatusPage',
       component: StatusPage
+    },
+    {
+      path: '/work',
+      name: 'WorkPage',
+      component: WorkPage
     }
   ],
   mode: 'history'
